a8de839b8d6c6c1f769fb7d8b542969d5e38e7dd
Zahra <bghzahra@outlook.com>
2022-03-11 22:05:56 +0000

    added return value to function

--- src/utils/isSpecialChar.ts
+++ src/utils/isSpecialChar.ts
@@ -1,4 +1,4 @@
-export default function isSpecialChar(word: string){
+export function isSpecialChar(word: string): boolean{
     const specialChars = ["#" , "!", "?"]
     for (const char of word){
         if (specialChars.includes(char)){
